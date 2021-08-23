a9228b911d71b280e1e60a9a53495f65111ed352
20anpeng07 <32548097+20anpeng07@users.noreply.github.com>
2021-08-23 14:19:47 -0400

    added google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -6,8 +6,33 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import theme from '../components/Theme';
 import '../styles/globals.css';
 
+
+
+import { useRouter } from 'next/router'
+import * as gtag from '../lib/gtag'
+
 export default function MyApp(props) {
   const { Component, pageProps } = props;
+
+  // google analytics
+  const router = useRouter()
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url)
+    }
+    //When the component is mounted, subscribe to router changes
+    //and log those page views
+    router.events.on('routeChangeComplete', handleRouteChange)
+
+    // If the component is unmounted, unsubscribe
+    // from the event with the `off` method
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+  // end google analytics
+
 
   React.useEffect(() => {
     // Remove the server-side injected CSS.
