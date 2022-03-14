d4a6d59eed9bd584dd6eea697ba0b290547067c8
William Barkoff <william@barkoffusa.com>
2022-03-14 19:40:30 -0400

    countdown updates

--- pages/countdown.js
+++ pages/countdown.js
@@ -4,6 +4,10 @@ import Head from '../components/layout/Head';
 import Footer from '../components/layout/Footer';
 import { useEffect, useState } from 'react';
 
+const MADNESS_GROUP_ID = "4559291"
+const MADNESS_URL = `https://fantasy.espncdn.com/tournament-challenge-bracket/2022/en/api/v7/group?groupID=${MADNESS_GROUP_ID}&sort=-1&start=0&length=50&periodPoints=false`
+
+
 const useStyles = makeStyles((theme) => ({
   text: {
     width: '90%',
@@ -12,6 +16,23 @@ const useStyles = makeStyles((theme) => ({
     left: '50%',
     transform: 'translate(-50%, -50%)',
     textAlign: "center",
+  },
+  textMadness: {
+    height: "30vh",
+    backgroundColor: "black",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center"
+  },
+  rocketryMadness: {
+    height: "70vh",
+    backgroundColor: "white",
+    padding: "20px",
+    color: "black"
+  },
+  madnessList: {
+    fontSize: "3em",
+    columnCount: 2,
   },
   countdown: {
     fontSize: 70,
@@ -45,6 +66,7 @@ export default function Countdown() {
   const classes = useStyles();
 
   const [countdown, setCountdown] = useState({ elapsed: true, days: 0, hours: 0, minutes: 0, seconds: 0 });
+  const [madnessData, setMadnessData] = useState(null);
 
   useEffect(() => {
     // assume monitor refresh rate is 120Hz: (120Hz)
@@ -53,10 +75,20 @@ export default function Countdown() {
     return () => { clearInterval(timer); clearInterval(pageReloadTimer) };
   }, [setCountdown])
 
+  useEffect(async () => {
+    if (new Date("2022-03-17T16:00:00.000Z") > new Date() && !(new URLSearchParams(window.location.search).has("showMadness"))) {
+      return
+    }
+    const response = await fetch(MADNESS_URL)
+    const data = await response.json()
+    console.log(data)
+    setMadnessData(data)
+  }, [setMadnessData])
+
   return (
     <div className={classes.root}>
       <Head title='Countdown' />
-      <div className={classes.text}>
+      <div className={`${madnessData ? classes.textMadness : classes.text}`} id="text">
         <Typography variant='h1' align='center' className={classes.countdown}>
           {countdown.elapsed ?
             <>Cornell Rocketry Team</> :
@@ -66,12 +98,18 @@ export default function Countdown() {
         <Typography variant='h3' align='center'>
           {!countdown.elapsed && "Dress Reharsal"}
         </Typography>
-        <img
+        {!madnessData && <img
           src='/static/crt.png'
           alt='logo'
           className={classes.logo}
-        />
+        />}
       </div>
+      {madnessData && <div className={classes.rocketryMadness}>
+        <Typography variant='h2'>&#x1F3C0; Rocketry Madness</Typography>
+        <ol className={classes.madnessList}>
+          {madnessData.g.e.slice(0, 16).map((data, i) => <li key={i}>{data.n_d} ({data.p})</li>)}
+        </ol>
+      </div>}
     </div>
   );
 }
