3446aed64b4847e00189f47854f11ccdd520d1b0
ancheypeng <32548097+20anpeng07@users.noreply.github.com>
2023-04-23 12:53:13 -0400

    updated styling

--- pages/faq.js
+++ pages/faq.js
@@ -1,50 +1,36 @@
-import { useRef, useEffect, useState } from 'react';
-import React from 'react';
+import { Container, Typography, makeStyles } from '@material-ui/core';
+
 import Accordion from '@material-ui/core/Accordion';
 import AccordionDetails from '@material-ui/core/AccordionDetails';
 import AccordionSummary from '@material-ui/core/AccordionSummary';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import {
-  Container,
-  makeStyles,
-  Typography,
-  Grid,
-  useMediaQuery,
-  useTheme,
-  Box,
-} from '@material-ui/core';
-import Header from '../components/layout/Header';
-import Stars from '../components/Stars';
-import Head from '../components/layout/Head';
-import Footer from '../components/layout/Footer';
 import { FaqList } from '../public/static/faq/faqlist';
-
+import Footer from '../components/layout/Footer';
+import Head from '../components/layout/Head';
+import Header from '../components/layout/Header';
+import React from 'react';
+import Stars from '../components/Stars';
 
 const useStyles = makeStyles((theme) => ({
   content: {
-    padding: '100px 50px 200px 50px',
+    padding: '100px 50px 100px 50px',
     [theme.breakpoints.only('xs')]: {
       padding: '80px 20px 80px 20px',
     },
+    minHeight: '100vh',
   },
   title: {
     marginTop: 20,
     marginBottom: 60,
   },
-  questionHeading: {
-    fontSize: 22,
+  question: {
+    fontSize: 20,
     fontWeight: 600,
   },
 }));
 
 export default function Faq() {
   const classes = useStyles();
-  const [expanded, setExpanded] = React.useState(false);
-
-
-  const handleChange = (panel) => (event, isExpanded) => {
-    setExpanded(isExpanded ? panel : false);
-  };
 
   return (
     <div className={classes.root}>
@@ -56,48 +42,32 @@ export default function Faq() {
         <Typography variant='h2' className={classes.title}>
           FAQ
         </Typography>
-        <FaqDisplay
-          data={FaqList}
-        />
+        <FaqDisplay data={FaqList} />
       </Container>
       <Footer />
     </div>
   );
+
   function FaqDisplay({ data }) {
     return (
       <>
-        {data.map((content, index) => (
-          <QnaDisplay
-            index={index}
-            qs={content.q}
-            ans={content.a}
-          />
+        {data.map((content) => (
+          <QnaDisplay question={content.q} answer={content.a} />
         ))}
       </>
     );
   }
 
-  function QnaDisplay({ index, qs, ans }) {
+  function QnaDisplay({ question, answer }) {
     return (
-      <Accordion expanded={expanded === index} onChange={handleChange(index)}>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panelbh-content"
-          id="panelbh-header"
-        >
-          <Typography className={classes.heading}></Typography>
-          <Typography className={classes.secondaryHeading}>
-            {qs}
-          </Typography>
+      <Accordion>
+        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+          <Typography className={classes.question}>{question}</Typography>
         </AccordionSummary>
         <AccordionDetails>
-          <Typography>
-            {ans}
-          </Typography>
+          <Typography>{answer}</Typography>
         </AccordionDetails>
       </Accordion>
-
     );
-  };
+  }
 }
-
