91db5d66899bee3f13f54110a9b07869780f8ee8
b0ss5 <35873851+b0ss5@users.noreply.github.com>
2026-02-23 02:28:23 -0500

    Fix typo in 'contributors' prop name

--- components/sponsors/SponsorLogos.js
+++ components/sponsors/SponsorLogos.js
@@ -12,7 +12,7 @@ export default function SponsorLogos({ imageDir, levels, contributers }) {
           sponsors={levels[levelName]}
         />
       ))}
-      <Contributers contributers={contributers} />
+      <Contributers contributers={contributors} />
     </>
   );
 }
